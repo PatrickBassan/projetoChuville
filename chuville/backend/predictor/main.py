8d0d9927f1eaab2ede0fbf3e09495845bc37c773
Patrick de Freitas Bassan <patrickfbassan@gmail.com>
2023-11-26 00:24:41 -0300

    Issue-14: Correção pylint

--- chuville/backend/predictor/main.py
+++ chuville/backend/predictor/main.py
@@ -9,15 +9,18 @@ import requests
 
 
 def send_msg(chat_id, probability, period, cdregion):
+    """Função que envia previsão de alagamento via bot do telegram"""
     token = "6340610289:AAFOptmbS9hWt-bWwA6dOFRDss4qNy84G1w"
     period = get_period_term(period)
     cep = get_cep(cdregion)
-    message = f"As chances de alagamentos para o CEP {cep[0]} são de {probability}% no período: {period}"
+    message = (f"As chances de alagamentos para o CEP {cep[0]}"
+               f" são de {probability}% no período: {period}")
     url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}"
     requests.get(url).json()
 
 
 def get_moon_phase(moon_phase):
+    """Função que retorna fase da lua em forma de número pra ser usada no preditor"""
     if moon_phase == 'new':
         phase = 1
     elif moon_phase == 'waxing_crescent':
@@ -41,6 +44,7 @@ def get_moon_phase(moon_phase):
 
 
 def get_period_term(period):
+    """Função que retorna período do dia com base no código"""
     if period == 1:
         term = 'Manhã'
     elif period == 2:
@@ -54,6 +58,7 @@ def get_period_term(period):
 
 
 def insert_forecast(prediction, period, cdregion):
+    """Função que salva a previsão de alagamento"""
     try:
         conn = mysql.connector.connect(
             host='localhost',
@@ -81,6 +86,7 @@ def insert_forecast(prediction, period, cdregion):
 
 
 def notify_users(cdregion, probability, period):
+    """Função responsável por verificar usuários que recebem notificação"""
     try:
         conn = mysql.connector.connect(
             host='localhost',
@@ -106,6 +112,7 @@ def notify_users(cdregion, probability, period):
 
 
 def get_cep(cdregion):
+    """Função que retorna o CEP com base no código da região"""
     try:
         conn = mysql.connector.connect(
             host='localhost',
@@ -129,18 +136,24 @@ def get_cep(cdregion):
     except RuntimeError:
         print(f"Erro na conexão com o banco: {RuntimeError}")
 
+    return None
+
 
 def api_request():
+    """Função que faz requisição de dados meteorológicos para a API HG Weather"""
     response = requests.get(
-        'https://api.hgbrasil.com/weather?woeid=455873&fields=only_results,rain,moon_phase&key=226c4975'
+        'https://api.hgbrasil.com/weather?'
+        + 'woeid=455873&fields=only_results,rain,moon_phase&key=226c4975'
     )
     try:
         return response.json()
     except KeyError:
         print(f"Erro na requisição da API HGWeather: {NameError}")
+        return None
 
 
 def generate_forecast(rain, sea, moon):
+    """Função que usa o modelo AM para prever alagamento"""
     with open('predictorModel', 'rb') as f:
         predictor = pickle.load(f)
 
@@ -148,12 +161,13 @@ def generate_forecast(rain, sea, moon):
 
 
 def main():
+    """Função de Execução principal do preditor"""
     result = api_request()
-    region = random.randint(1, 244)
     period_count = 1
     while period_count != 5:
         moon = get_moon_phase(result['moon_phase'])
         sea = random.randint(0, 200)
+        region = random.randint(1, 244)
         forecast = generate_forecast(result['rain'], sea, moon)
         insert_forecast(forecast, period_count, region)
         period_count += 1
