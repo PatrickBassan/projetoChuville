e9f58f40b5961f98ce695392c3882e31276fc022
Patrick de Freitas Bassan <patrickfbassan@gmail.com>
2023-11-27 23:16:31 -0300

    Issue-26: Ajustes

--- chuville/backend/api/db.js
+++ chuville/backend/api/db.js
@@ -1,8 +1,8 @@
 import mysql from "mysql";
 
 export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "111111",
-    database: "chuville"
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
 })
